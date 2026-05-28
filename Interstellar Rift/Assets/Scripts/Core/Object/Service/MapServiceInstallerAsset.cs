341d8b039a79c4f68f5311c3a36df097fb406480
GloryDay <alwaysgloryday@gmail.com>
2026-05-28 17:15:18 +0900

    feature: add map service installation asset

---
+++ Interstellar Rift/Assets/Scripts/Core/Object/Service/MapServiceInstallerAsset.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using VContainer;
+
+namespace Core.Object.Service
+{
+    [CreateAssetMenu(fileName = "Map Service Installer", menuName = "Scriptable Objects/Services/Installer/Map")]
+    public class MapServiceInstallerAsset : ServiceInstallerAsset
+    {
+        public override void Install(IContainerBuilder builder)
+        {
+
+        }
+    }
+}
