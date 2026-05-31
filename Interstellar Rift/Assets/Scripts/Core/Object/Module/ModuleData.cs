96fe150f6987c2483c5f3e715f97266ff65925c6
GloryDay <alwaysgloryday@gmail.com>
2026-06-01 05:02:00 +0900

    feature: add rank data to the module data

--- Interstellar Rift/Assets/Scripts/Core/Object/Module/ModuleData.cs
+++ Interstellar Rift/Assets/Scripts/Core/Object/Module/ModuleData.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 namespace Core.Object.Module
 {
+    [CreateAssetMenu(fileName = "Module Data", menuName =  "Scriptable Objects/Data/Module/Test")]
     public class ModuleData : ScriptableObject
     {
         #region SERIALIZABLE PROPERTY API
 
         [Title("Default")]
+        [ShowInInspector] public Rank Rank { get; set; }
         [ShowInInspector] public float Mass { get; set; }
 
         #endregion
---
+++ Interstellar Rift/Assets/Scripts/Core/Object/Module/Rank.cs
@@ -0,0 +1,17 @@
+namespace Core.Object.Module
+{
+    public enum Rank
+    {
+        Alpha,
+        Beta,
+        Gamma,
+        Delta,
+        Epsilon,
+        Zeta,
+        Eta,
+        Theta,
+        Iota,
+        Kappa,
+        Lambda,
+    }
+}
